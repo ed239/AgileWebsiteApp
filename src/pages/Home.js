1fec7da5f4d99e80ce3934aa987e126460bbb622
Eden Dubrovsky <ed239@njit.edu>
2023-09-29 22:05:05 -0400

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,12 +37,15 @@ export default function Home(){
                 <div class='column'>
                 <div class='grey-column'>
                     <br></br>
-                    <h2>Courses</h2>
+                    <h2>Assessment</h2>
                     <br></br>
                     <img class='colImg' src='https://www.freeiconspng.com/thumbs/book-icon/book-stack-icon--icon-search-engine-16.png'></img>
                     <br></br>
                     <br></br>
-                    <p> Info about courses</p>
+                    <p> Apex agile experts can perform an Agile Maturity Assessment for your company. We thoroughly
+                        study your business organization and measure how the software development practices you
+                        follow conform to the <a href="https://www.agilealliance.org/agile101/12-principles-behind-the-agile-manifesto/">Agile Manifesto's 12 Agile Principles.</a> 
+                    </p>
                 </div>
                 </div>
                 <div class='column'>
@@ -53,34 +56,90 @@ export default function Home(){
                     <img class='colImg' src='https://freepngimg.com/save/29395-analyst-photos/512x512'></img>
                     <br></br>
                     <br></br>
+                    <p>Working one on one with your own technology leaders, Apex experts share our decades of Agile  expertise,
+                     with just the right level of sensitivity and suitability for your business needs. </p>
                 </div>
                 </div>
                     <div class='column'>
                 <div class='grey-column'>
                     <br></br>
-                    <h2>Training</h2>
+                    <h2>Courses</h2>
                     <br></br>
                     <img class='colImg' src='https://www.freeiconspng.com/thumbs/laptop-icon/laptop-icon-24.png'></img>
                     <br></br>
                     <br></br>
+                    <p>Apex coursework will thoroughly prepare you for Agile certification. Having worked side-by side with the three 
+                    elite certification agencies, we know just what you will need to gain  certification from the three important certification organizations SAFe, Scrum.org, and Scrum  Alliance.</p>
                 </div>
                 </div>
             </div>
         </div>
         <br></br>
         <br></br>
-        <h1>About Us</h1>
-        <br></br>
-        <br></br>
 
-        
-        <div class="aboutUsS" >
-            <div class = "rowS"><b>Courses</b></div>
-            <div class = "rowS"><b>Coaching</b></div>
-            <div class = "rowS"><b>Training</b></div>
-            <p class ="ts"> Our classes are about.....</p>
+        <div class='some-page-wrapper'>
+            <div class='row2'>
+                <div class='column'>
+                <div class='white-column'>
+                    <br></br>
+                    <br></br>
+                    <h1>About Us</h1>
+                    <br></br>
+                    <br></br>
+                    <p> Apex is a boutique Agile consultancy with decades of software development experience. We  grew up in the era of structured design 
+                    methodologies, adapted our learning to the rapid  development approaches, and graduated to Agile. Wherever you are in your software evolution,  
+                    we are there to help in your journey.
+                    </p>
+                </div>
+                </div>
+                <div class='column'>
+                <div class='white-column'>
+                    <br></br>
+                    <br></br>
+                    <img class='aboutImg' src='https://as1.ftcdn.net/v2/jpg/01/77/91/20/1000_F_177912003_ncVNX7GxPR6Z2NYcHvJTGPbrD26BcVKF.jpg'></img>
+                </div>
+                </div>
+            </div>
         </div>
-
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h1>Our Partners</h1>
+        <br></br>
+        <div class='some-page-wrapper'>
+            <div class='row2'>
+                <div class='column'>
+                <div class='white-column'>
+                    <br></br>
+                    <h3>SAFe</h3>
+                    <br></br>
+                    <img class='partnerImg' src='https://images.credly.com/images/7a30a2e5-82bb-406b-b0f8-52fd846eac69/linkedin_thumb_cert_mark_SSM_badge_large_300px.png'></img>
+                    <br></br>
+                </div>
+                </div>
+                <div class='column'>
+                <div class='white-column'>
+                    <br></br>
+                    <h3>Scrum Alliance</h3>
+                    <br></br>
+                    <img class='partnerImg' src='https://theme.zdassets.com/theme_assets/67158/c28adb2e3532619ebc9a9072599677f56ebee94b.png'></img>
+                    <br></br>
+                </div>
+                </div>
+                <div class='column'>
+                <div class='white-column'>
+                    <br></br>
+                    <h3>Scrum.org</h3>
+                    <br></br>
+                    <img class='partnerImg' src='https://devon.global/wp-content/uploads/2018/11/scrumorg-logo.png'></img>
+                    <br></br>
+                </div>
+                </div>
+            </div>
+        </div>
+        
+        
         
 
        </body>
