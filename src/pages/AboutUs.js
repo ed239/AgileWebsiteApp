3e745f12d289e5194deb9864395698aa7340dabc
Eden Dubrovsky <ed239@njit.edu>
2023-10-02 15:20:19 -0400

    AboutUs Page and NavBar fix

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,4 +1,74 @@
+import './pages.css'
+
 export default function AboutUs(){
-    return <h1>AboutUs</h1>
+    return (
+        <body class="body">
+            <h1>About Us</h1>
+            <div class="row">
+                <div class='grey-columnAU'>
+                        <br></br>
+                        <br></br>
+                        <h2>Who We Are</h2>
+                        <br></br>
+                        <p>Apex is a boutique consulting firm with decades of software development experience. We grew  up in the era of structured design methodologies, adapted 
+                        our learnings to the rapid development  approaches, and graduated to Agile. Having partnered with many of the original Agile founders,
+                         we worked hand-in-hand with them  to implement Agile software development practices at companies like IBM, Lotus and Verizon.  Raj Heda, 
+                         President and CEO of Apex, is a leading author, teacher and world-renowned industry  consultant. Our in-house experts are master’s degree 
+                         students at Boston University’s  Metropolitan College, with decades of industry experience.</p>
+                         <br></br>
+                         <ul style={{marginLeft: '2.1em'}}>
+                            <li>Devin Dumais, Andras Palfi, and Shailesh Tamrakar are Apex’s three lead development  consultants. </li>
+                            <li>Keith Gutfreund leads Apex’s Product Owner and Content Writer teams. Keith studied  Electrical and 
+                                Computer Engineering and has been a hardware and software developer for  over 4 decades. </li>
+                            <li>Po-Han (John) Huang and Daniela lead Apex’s Quality certification team. • Mingyang Zu is lead consultant for Apex’s Business Analyst certification team.</li>
+                        </ul>
+                </div>
+                <div class='grey-columnAU'>
+                        <br></br>
+                        <br></br>
+                        <img class="AUimg" src="https://as1.ftcdn.net/v2/jpg/01/52/05/90/1000_F_152059080_aGjnTtv3MhoRzt7OYlsqcQl6g81vC6Gc.jpg"></img>
+                        <br></br>
+                        <p></p>
+                </div>
+            </div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <div class="row">
+                <div class='grey-columnAU'>
+                        <br></br>
+                        <br></br>
+                        <img class="AUimg2" src="https://as1.ftcdn.net/v2/jpg/00/95/05/16/1000_F_95051652_OfxTtF2oW8W6A9FBFJHhZJCKeajQDYnS.jpg"></img>
+                        <br></br>
+                        
+                        <p></p>
+                </div>
+                <div class='grey-columnAU'>
+                        <br></br>
+                        <br></br>
+                        <h2>What We Do</h2>
+                        <br></br>
+                        <h4>Apex can help you succeed in three ways: </h4>
+                        <br></br>
+                        <ul style={{marginLeft: '2.1em'}}>
+                            <li>Training & Certification: our 1-2 day training courses are specifically designed to prepare  you for the Agile certification exams from SAFe, 
+                                Scrum.org, and Scrum Alliance. You  can train with confidence due to our Apex guarantee: if you do not succeed in passing the  certification, 
+                                you may re-enroll in the training at no additional cost.  </li>
+                            <li>Coaching: Apex offers customized coaching from our expert staff. We can coach your  team, your executives, and your organization. 
+                                Please contact us for more details.</li>
+                            <li>Assessment: Apex uniquely offers a suite of detailed assessments to determine your  
+                               specific organization’s capabilities and needs. See our different assessment offerings 
+                               here  (click link to Assessment page) for more details.</li>
+                        </ul>
+                        <br></br>
+                        <br></br>
+                        <br></br>
+                </div>
+            </div>
+            <br></br>
+            <br></br>
+            <br></br>
+        </body>
+        )
 
 }
---
+++ src/pages/NewCourses.js
@@ -0,0 +1,13 @@
+import './pages.css'
+
+export default function Assessment(){
+
+    return (
+    <body class="body">
+        <h1>New Courses</h1>
+        <p>Text</p>
+
+    </body>
+    )
+
+}
