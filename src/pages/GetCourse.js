ac3967642cba2c87e613db14b1fff2d8a8d1a018
Eden Dubrovsky <ed239@njit.edu>
2023-10-07 19:41:13 -0400

    Training page - public

--- src/pages/GetCourse.js
+++ src/pages/GetCourse.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useParams, useNavigate} from 'react-router-dom';
+import './pages.css'
 
 function GetCourse() {
   const navigate = useNavigate();
