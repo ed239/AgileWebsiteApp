048599583ecc9d2d727d76fc4cbe94e562188e6d
Eden Dubrovsky <55884703+ed239@users.noreply.github.com>
2023-10-23 16:09:20 -0400

    Revert "Master"

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,22 +1,4 @@
-import './pages.css'
-
 export default function ContactUs(){
-    return (
-
-        <div className="contact-box">
-           
-            <h1>How to Contact Us</h1>
-            <p>
-                If you have any questions about Easy e-learning’s privacy policy, the data we hold on you,
-                 or you would like to exercise one of your data protection rights, please do not hesitate 
-                 to contact us. 
-
-            </p>
-            <br></br>
-            <p><b>Email us at:</b> <span className='ctn-email'>info@eelagility.com</span></p>
-        </div>
-    )
-    
-    
+    return <h1>Contact Us</h1>
 
 }
