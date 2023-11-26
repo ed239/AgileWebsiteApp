c43e670288188c6d29df1e0c8ee33b0b76bb630d
TiylLg <82725236+TiylLg@users.noreply.github.com>
2023-11-26 14:50:19 -0500

    Revert "ScrollToTop, Contact us"

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -3,9 +3,93 @@ import pic5 from "./images/5.jpeg";
 import FormControl from '@mui/material/FormControl';
 import React, { useState } from 'react';
 
-
-
 export default function ContactUs(){
+    const [showPopup, setShowPopup] = useState(false);
+
+    function ClosePopup(){
+        setShowPopup(false);
+        window.location.reload();
+    }
+
+    function ValidationResult(){
+        const payload = {
+            'name':document.getElementById('name').value,
+            'email':document.getElementById('email-address').value,
+            'phone':document.getElementById('phone-number').value,
+            'message':document.getElementById('message').value,
+        };
+        const validateEmail = (payload) => {
+            const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return regex.test(payload['email']);
+          };
+    
+        const validatePhoneNumber = (payload) => {
+            const regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+            return regex.test(payload['phone']);
+        };
+    
+        const validateFullName = (payload) => {
+            const regex = /^[a-z ]+$/i;
+            const lengthWithoutSpaces = payload['name'].replace(/\s/g, '').length;
+            return regex.test(payload['name']) && lengthWithoutSpaces >= 2;
+        };
+    
+        if (!validateEmail(payload)) {
+          return 'Please enter valid email address';
+        }
+        if (!validatePhoneNumber(payload)) {
+          return 'Please enter 10 digit phone number';
+        }
+        if (!validateFullName(payload)) {
+          return 'Please enter valid fullname with no special character. Minimum two characters';
+        }
+        else{
+            return 'Sign Up Successful!';
+        }
+    }
+
+
+
+    const handleSubmit = async (event) => {
+        
+        event.preventDefault(); // Prevents the default form submission behavior
+        setShowPopup(true);
+        if(ValidationResult() !="Sign Up Successful!"){
+            return
+        } 
+    
+        // Extract form data
+        const formData = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email-address').value,
+            phone: document.getElementById('phone-number').value,
+            message: document.getElementById('message').value
+        };
+    
+        // Make the POST request
+        try {
+            const response = await fetch('http://localhost:5000/authentication-swagger/v1/send-email', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'accept': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+    
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+    
+            // Process response here
+            const data = await response.json();
+            console.log('Success:', data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+        
+
+    };
 
     return (
         <div className="contact-box">
@@ -17,7 +101,6 @@ export default function ContactUs(){
               </div>
               <div>
                 <FormControl variant='standard' className='cnt-form'>
-                    <h2>Contact Us</h2>
                     <label>Name</label>
                     <input type='text' id="name" name='name'/>
                     <label>Email Address</label>
